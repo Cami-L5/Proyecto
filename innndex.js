f680208d4deb395a0741f26cc3009614773c7231
Cami-L5 <C.Lorefice@colegiocalasanz.edu.ar>
2025-10-02 18:32:39 -0300

    Add files via upload

---
+++ innndex.js
@@ -0,0 +1,14 @@
+function jugar() {
+    const preguntas = [
+  {
+    pregunta: "¿Cuál es la capital de Francia?",
+    opciones: ["París", "Londres", "Berlín", "Roma"],
+    respuestaCorrecta: 0
+  },
+  {
+    pregunta: "¿Cuál es el planeta más grande del sistema solar?",
+    opciones: ["Tierra", "Saturno", "Júpiter", "Urano"],
+    respuestaCorrecta: 2
+  }
+];
+}
